9470b4224ca279bf97c95f1cbc22b53c995f2d03
nishanth0802 <144027624+nishanth0802@users.noreply.github.com>
2023-09-09 15:30:08 +0530

    Add files via upload

---
+++ newto.js
@@ -0,0 +1,53 @@
+function add()
+{
+     let taskinput= document.getElementById("input");
+    let tasktext= taskinput.value.toString();
+    
+if(tasktext===" ")
+{
+    alert("EMPTY TASKS CANNOT BE ");
+return;
+}
+
+
+let tasklist =document.getElementById("listb");
+let taskitem=document.createElement("li"); 
+taskitem.innerHTML=`<span> ${tasktext}</span>
+
+<button class="delbutton" onclick="deletebutton(this)">del</button>
+`;
+
+tasklist.appendChild(taskitem);
+
+
+taskinput.value=" ";
+
+
+taskitem.addEventListener("click", function donebutton () {
+    let span = this.querySelector('span');
+    if (span.style.textDecoration === "line-through") {
+        span.style.textDecoration = "none";
+    } else {
+        span.style.textDecoration = "line-through";
+    }
+});
+
+
+
+
+}
+
+
+function deletebutton(button){
+    let tasklist = document.getElementById("listb");
+    let taskitem = button.parentElement;
+    tasklist.removeChild(taskitem);
+}
+
+
+
+   
+
+
+
+
